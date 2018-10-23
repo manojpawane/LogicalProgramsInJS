9e3fa27f8e1d9be17e9a9659175c2a29c02f8ddc
manoj <manojpawane@gmail.com>
2018-10-23 12:55:33 +0530

    prime factor

--- PrimeFactor.js
+++ PrimeFactor.js
@@ -18,16 +18,17 @@ PrimeFactor =  function()
     }
     else
     {
-        for(var i= 1; i<= response;i++)
+        for(var i= 1; i<= response/2;i++)
         {
             if(response%i === 0)
             {
                 var flag=0;
-                for(var j=2;j<=i;j++)
+                for(var j=2;j<i;j++)
                 {
                     if(i%j===0)
                     {
-                        flag =0;
+                        flag =1;
+                        break;
                     }
                 }
                 if(flag === 0)
