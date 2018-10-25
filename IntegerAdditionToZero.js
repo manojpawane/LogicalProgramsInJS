c86d24993f320d998215cf8db2be82321d4b60af
manoj <manojpawane@gmail.com>
2018-10-25 18:40:11 +0530

    IntegerAdditiontoZero completed

--- IntegerAdditionToZero.js
+++ IntegerAdditionToZero.js
@@ -10,6 +10,7 @@ var response
 var data
 rl.question('Please enter the size of array.', async (answer) => {
 	console.log('Entered value ', answer);
+	response = answer
 	console.log('Please enter the element: ');
 	var arrayData = new Array(answer);
 	for (var i = 0; i < answer; i++) {
@@ -19,12 +20,29 @@ rl.question('Please enter the size of array.', async (answer) => {
 			console.log(err);
 		});
 	}
-	DisplayData(arrayData);
+	AdditionToZero(arrayData);
 	rl.close();
 });
 
-var DisplayData = function (dataFromMethodToDisplay) {
-	console.log(dataFromMethodToDisplay);
+
+var AdditionToZero = function (arr) {
+	var count = 0;
+	for(var j = 0 ; j < response ; j++)
+		{
+			for(var k = 0 ; k < response ; k++)
+			{
+				for(var l = 0 ; l < response ; l++)
+				{
+					if( (arr[j]*1) + (1*arr[k]) + (1*arr[l]) === 0)
+					{
+						console.log("Sum of "+arr[j]+" "+arr[k]+" "+arr[l]+" is Equal to Zero" );
+						count++;
+					}
+				}
+			}
+
+		}
+console.log("The no. of distinct varaible: " +count);
 }
 
 
