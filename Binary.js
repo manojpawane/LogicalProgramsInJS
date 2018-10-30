c932047cfaa93507a644b41e490835c705870e7f
manojpawane <manojpawane@gmail.com>
2018-10-30 19:56:29 +0530

    binary.js added

--- Binary.js
+++ Binary.js
@@ -7,9 +7,26 @@ var rl = readline.createInterface(
 )
 
 var response
+var firstNibble;
+var secondNibble;
+var swapNibble;
 rl.question('Please enter number to find binary representation. ',(answer)=>{
  response = answer
  var data = BinaryRepresentation.binaryRepresentation();
+ console.log(data);
+ var firstNibble = data.slice(0,4);
+ var secondNibble = data.slice(4,9);
+ var swapNibble = secondNibble + firstNibble;
+
+ console.log(swapNibble);
+ var dataSwap=  Math.log2(data) % 1;
+ if(dataSwap === 0)
+ {
+     console.log('After swapping ' +swapNibble+ 'the integer  + ' is Power of 2')
+ }
+ else{
+    console.log('After swapping the integer '+swapNibble + ' is not Power of 2')
+ }
  console.log(data);
  rl.close();
 })
