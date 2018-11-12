496aaaf4426c43fb2c57d96117aa290a2980f201
manojpawane <manojpawane@gmail.com>
2018-11-13 00:15:11 +0530

    primeanagram

---
+++ Data Structures/PrimeAnagram.js
@@ -0,0 +1,46 @@
+var readline = require('readline');
+var rl = readline.createInterface(
+    {
+        input:process.stdin,
+        output:process.stdout
+    }
+);
+
+rl.question('Please enter the minimum number of the range: ', (answer)=>{
+    rl.question('please enter the maximum number of the range: ',async (answer2)=>{
+       var primeNumber = await EvaluatePrimeNumber(answer, answer2)
+       var lengthOFArray = primeNumber.length;
+       console.log('min range ' +answer);
+       console.log('max range ' +answer2);
+       console.log('Prime Number: '+primeNumber);
+       rl.close();
+    })
+})
+
+var EvaluatePrimeNumber = function(minRange, maxRange){
+return new Promise(function(resolve, reject){
+         try {
+             var flag;
+             var k=0;
+             var primeNumber = new Array(maxRange);
+             for(var i = (minRange*1) +1 ; i < maxRange ;  ++i){
+             flag = 0;
+             for(var j=2;j<=i/2;++j){
+                if(i % j == 0)
+                {
+                   flag = 1;
+                   break;
+                }
+            }
+            if(flag == 0)
+            {
+                primeNumber[k] = i;
+                k++;
+            }
+        } 
+         resolve(primeNumber);
+     } catch (error) {
+         reject(error)
+     }
+    })  
+}
