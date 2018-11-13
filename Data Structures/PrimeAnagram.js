fdd93c4078b8615bd1ffa6e5146a20a09cb3dc15
manoj <manojpawane@gmail.com>
2018-11-13 10:20:17 +0530

    Prime Anagram added

--- Data Structures/PrimeAnagram.js
+++ Data Structures/PrimeAnagram.js
@@ -6,16 +6,40 @@ var rl = readline.createInterface(
     }
 );
 
+var array3 = new Array();
+var unique = [];
 rl.question('Please enter the minimum number of the range: ', (answer)=>{
     rl.question('please enter the maximum number of the range: ',async (answer2)=>{
        var primeNumber = await EvaluatePrimeNumber(answer, answer2)
-       var lengthOFArray = primeNumber.length;
-       console.log('min range ' +answer);
-       console.log('max range ' +answer2);
-       console.log('Prime Number: '+primeNumber);
+       var array4 = new Array();
+       var anagramInRange = await anagramChecker(primeNumber);
+       array4.push(anagramInRange);
+       await unqiueData(primeNumber, array3);
+       array4.push(unique);
+       console.log('First row represent the number which are anagram and second represent which are not anagram: ')
+       for(var m = 0; m < array4.length; m++){
+        console.log(array4[m].join(' '));
+    }
        rl.close();
     })
 })
+
+var unqiueData =  function(array1, array2){
+
+for(var i = 0; i < array1.length; i++){
+    var found = false;
+
+    for(var j = 0; j < array2.length; j++){ // j < is missed;
+     if(array1[i] == array2[j]){
+      found = true;
+      break; 
+    }
+   }
+   if(found == false){
+   unique.push(array1[i]);
+  }
+}
+}
 
 var EvaluatePrimeNumber = function(minRange, maxRange){
 return new Promise(function(resolve, reject){
@@ -43,4 +67,26 @@ return new Promise(function(resolve, reject){
          reject(error)
      }
     })  
+}
+
+var anagramChecker =  function(arr){
+    var arr1 = new Array();
+    var x = 0;
+    for (var i = 0; i < arr.length; i++) {
+        for (var j = i + 1; j < arr.length; j++) {
+            var a1 = '' + parseInt(arr[i]);
+            var a2 = '' + parseInt(arr[j]);
+            if ((((a1.split('')).sort()).join()) === (((a2.split('')).sort()).join())) //checking for anagram
+            {
+                str = (a1+ ' '+a2);
+                arr1.push(str);
+                str = "";
+                array3[x] = a1;
+                x++;
+                array3[x] = a2;
+                x++;
+            }
+        }
+}
+return arr1;
 }
