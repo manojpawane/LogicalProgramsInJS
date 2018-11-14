95c99760a281612bbeafa00b5f779d68a18dfc01
manoj <manojpawane@gmail.com>
2018-11-14 10:55:57 +0530

    Inventory management first commit

---
+++ OOPS/InventoryManagement.js
@@ -0,0 +1,49 @@
+'use strict';
+const fs = require('fs');
+
+var readFileForJson = function(){
+    return new Promise(function(resolve, reject){
+        try {
+            fs.readFile('Inventory.json',(err, data)=>{
+                if(err){
+                    throw err;
+                }
+                else{
+                    let datal = JSON.parse(data);
+                    resolve(datal);
+                }
+            })
+        } catch (error) {
+            reject(error);
+        }
+    })
+}
+
+var startUp =async function(){
+    var dataFromFile = await readFileForJson();
+    // console.log(dataFromFile.Rice[0].Price);
+    console.log('Name   Price   Quantity   Total');
+    
+    for(let i = 0; i < dataFromFile.Rice.length; i++){
+        console.log(dataFromFile.Rice[i].name  +  '   ' +
+                     dataFromFile.Rice[i].price  + '      ' +
+                     dataFromFile.Rice[i].weight   + '        '+
+                     dataFromFile.Rice[i].weight * dataFromFile.Rice[i].price);
+    }
+    console.log('\n');
+    for(let i = 0; i < dataFromFile.Wheat.length; i++){
+        console.log(dataFromFile.Wheat[i].name  +  '   ' +
+                     dataFromFile.Wheat[i].price  + '      ' +
+                     dataFromFile.Wheat[i].weight   + '        '+
+                     dataFromFile.Wheat[i].weight * dataFromFile.Wheat[i].price);
+    }
+    console.log('\n');
+    for(let i = 0; i < dataFromFile.Pulses.length; i++){
+        console.log(dataFromFile.Pulses[i].name  +  '   ' +
+                     dataFromFile.Pulses[i].price  + '      ' +
+                     dataFromFile.Pulses[i].weight   + '        '+
+                     dataFromFile.Pulses[i].weight * dataFromFile.Pulses[i].price);
+    }
+}
+
+startUp();
